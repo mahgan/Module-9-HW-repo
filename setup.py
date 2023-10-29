fd13198ab2b689efa0e0f005a533246c352af147
Mahin_Ganesan <mahinganesan23@gmail.com>
2023-10-29 12:39:57 -0400

    Changes Made

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+
+setup(
+
+    name='booklover',
+
+    version='1.0.0',
+
+    url='https://github.com/mahgan/Module-9-HW-repo',
+
+    author='Mahin Ganesan',
+
+    author_email='mg4ccz@virginia.edu',
+
+    description='Package contains modules BookLover and BookLover_test which creates a class for book lovers and tests it.',
+
+    license='MIT',
+
+    packages=['booklover'],    
+
+    install_requires=['pandas','numpy']
+
+)
